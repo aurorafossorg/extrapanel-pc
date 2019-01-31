dd71021143b1118a62c1ddd79a4dae914518c4a6
Ev1lbl0w <ricasubtil@gmail.com>
2019-01-31 22:17:36 +0000

    Implemented GUI in code and some interactivity

--- src/app/app.d
+++ src/app/app.d
@@ -4,6 +4,7 @@ module extrapanel.app;
 import std.stdio;
 import std.file;
 import std.experimental.logger;
+import std.conv;
 
 // GTK
 // Types
@@ -15,30 +16,23 @@ import gtk.ApplicationWindow;
 import gtk.Widget;
 import gtk.Window;
 
+import pango.PgAttributeList;
+import pango.PgAttribute;
+
 // Contruct
 import gtk.Builder;
 
 // Elements
-import gtk.Box;
 import gtk.Button;
-import gtk.ButtonBox;
-import gtk.DrawingArea;
-import gtk.Fixed;
-import gtk.HeaderBar;
-import gtk.Image;
 import gtk.Label;
-import gtk.MenuButton;
-import gtk.Overlay;
-import gtk.Popover;
-import gtk.ProgressBar;
-import gtk.Scale;
-import gtk.SpinButton;
-import gtk.Spinner;
-import gtk.Switch;
-
-// 
-// Gio
-// 
+import gtk.Stack;
+import gtk.ListBox;
+import gtk.ListBoxRow;
+import gtk.Box;
+import gtk.Notebook;
+import gtk.ScrolledWindow;
+import gtk.CheckButton;
+import gtk.ToggleButton;
 
 // Top level
 import gio.Application : GApplication = Application;
@@ -50,6 +44,15 @@ import gio.MenuItem : GMenuItem = MenuItem;
 // GDK
 import gdkpixbuf.Pixbuf;
 import gdkpixbuf.PixbufLoader;
+
+
+enum State {
+	Online,
+	Offline,
+	Disabled
+}
+
+static State wifiState = State.Disabled, bluetoothState = State.Disabled, usbState = State.Disabled;
 
 /// Main application
 class ExtraPanelGUI : Application
@@ -82,17 +85,56 @@ private:
 
 			// Setup the app
 			initElements();
+			updateElements();
 		}
 
 		// Show
 		this.window.present();
 	}
 
-	// Contruct
+	// Constructor
 	Builder builder;
 
 	// Window
 	ApplicationWindow window;
+
+	// Meta Elements
+	Button backButton, startButton, stopButton;
+	Label status;
+
+	// Sidebar
+	Stack sidebar;
+		ListBox generalBar;
+			ListBoxRow gPluginsOption;
+			ListBoxRow gConfigOption;
+			ListBoxRow gAboutOption;
+		ListBox pluginsBar;
+			ListBoxRow pPluginsOption;
+			ListBoxRow pPacksOption;
+			ListBoxRow pInstalledOption;
+		ListBox configBar;
+			ListBoxRow cGeneralOption;
+			ListBoxRow cConnectionOption;
+			ListBoxRow cPluginsOption;
+			ListBoxRow cDevicesOption;
+
+	// Main Interface
+	Stack mainInterface;
+		Box generalInterface;
+			Button wifiButton, bluetoothButton, usbButton;
+		Stack pluginsInterface;
+		Stack configInterface;
+			ScrolledWindow cGeneralInterface;
+			Notebook cConnectionInterface;
+				Box ccWifiInterface;
+					CheckButton ccwEnableCheck;
+				Box ccBluetoothInterface;
+					CheckButton ccbEnableCheck;
+				Box ccUsbInterface;
+					CheckButton ccuEnableCheck;
+			Box cPluginsInterface;
+			Box cDevicesInterface;
+
 
 	// Inits the builder defined elements
 	void initElements()
@@ -100,5 +142,170 @@ private:
 		// Top level
 		window = cast(ApplicationWindow) builder.getObject("window");
 		window.setApplication(this);
+
+		// Meta Elements
+		backButton = cast(Button) builder.getObject("backButton");
+		startButton = cast(Button) builder.getObject("startButton");
+		stopButton = cast(Button) builder.getObject("stopButton");
+		status = cast(Label) builder.getObject("status");
+
+		// General Tab Buttons
+		wifiButton = cast(Button) builder.getObject("wifiButton");
+		bluetoothButton = cast(Button) builder.getObject("bluetoothButton");
+		usbButton = cast(Button) builder.getObject("usbButton");
+
+		// Sidebar
+		sidebar = cast(Stack) builder.getObject("sidebar");
+		generalBar = cast(ListBox) builder.getObject("generalBar");
+		gPluginsOption = cast(ListBoxRow) builder.getObject("gPluginsOption");
+		gConfigOption = cast(ListBoxRow) builder.getObject("gConfigOption");
+		gAboutOption = cast(ListBoxRow) builder.getObject("gAboutOption");
+		pluginsBar = cast(ListBox) builder.getObject("pluginsBar");
+		pPluginsOption = cast(ListBoxRow) builder.getObject("pPluginsOption");
+		pPacksOption = cast(ListBoxRow) builder.getObject("pPacksOption");
+		pInstalledOption = cast(ListBoxRow) builder.getObject("pInstalledOption");
+		configBar = cast(ListBox) builder.getObject("configBar");
+		cGeneralOption = cast(ListBoxRow) builder.getObject("cGeneralOption");
+		cConnectionOption = cast(ListBoxRow) builder.getObject("cConnectionOption");
+		cPluginsOption = cast(ListBoxRow) builder.getObject("cPluginsOption");
+		cDevicesOption = cast(ListBoxRow) builder.getObject("cDevicesOption");
+
+		// Main Interface
+		mainInterface = cast(Stack) builder.getObject("mainInterface");
+		generalInterface = cast(Box) builder.getObject("generalInterface");
+		pluginsInterface = cast(Stack) builder.getObject("pluginsInterface");
+		configInterface = cast(Stack) builder.getObject("configInterface");
+		cGeneralInterface = cast(ScrolledWindow) builder.getObject("cGeneralInterface");
+		cConnectionInterface = cast(Notebook) builder.getObject("cConnectionInterface");
+		ccWifiInterface = cast(Box) builder.getObject("ccWifiInterface");
+		ccwEnableCheck = cast(CheckButton) builder.getObject("ccwEnableCheck");
+		ccBluetoothInterface = cast(Box) builder.getObject("ccBluetoothInterface");
+		ccbEnableCheck = cast(CheckButton) builder.getObject("ccbEnableCheck");
+		ccUsbInterface = cast(Box) builder.getObject("ccUsbInterface");
+		ccuEnableCheck = cast(CheckButton) builder.getObject("ccuEnableCheck");
+		cPluginsInterface = cast(Box) builder.getObject("cPluginsInterface");
+		cDevicesInterface = cast(Box) builder.getObject("cDevicesInterface");
+	}
+
+	void updateElements()
+	{
+		setConnectionButtonState(wifiButton, wifiState);
+		setConnectionButtonState(bluetoothButton, bluetoothState);
+		setConnectionButtonState(usbButton, usbState);
+
+		generalBar.addOnRowActivated(&sidebarOnChange);
+		pluginsBar.addOnRowActivated(&sidebarOnChange);
+		configBar.addOnRowActivated(&sidebarOnChange);
+
+		wifiButton.addOnClicked(&communicationButtonCallback);
+		bluetoothButton.addOnClicked(&communicationButtonCallback);
+		usbButton.addOnClicked(&communicationButtonCallback);
+
+		ccwEnableCheck.addOnToggled(&ccEnableBoxes);
+		ccbEnableCheck.addOnToggled(&ccEnableBoxes);
+		ccuEnableCheck.addOnToggled(&ccEnableBoxes);
+
+		backButton.addOnClicked(&backButtonCallback);
+	}
+
+	void setConnectionButtonState(Button button, State state) {
+		string id;
+		if(button == wifiButton)
+			id = "wifiButton";
+		else if(button == bluetoothButton)
+			id = "bluetoothButton";
+		else if(button == usbButton)
+			id = "usbButton";
+
+		switch(state) {
+			case State.Online:
+				button.setSensitive(true);
+				Label label = cast(Label) builder.getObject(id ~ "Label");
+				label.setLabel("Active");
+				PgAttributeList attrs = label.getAttributes() is null ? new PgAttributeList() : label.getAttributes();
+				attrs.change(PgAttribute.foregroundNew(0x4e4e, 0x9a9a, 0x0606));
+				label.setAttributes(attrs);
+
+				break;
+			case State.Offline:
+				button.setSensitive(true);
+				Label label = cast(Label) builder.getObject(id ~ "Label");
+				label.setLabel("Offline");
+				PgAttributeList attrs = label.getAttributes() is null ? new PgAttributeList() : label.getAttributes();
+				attrs.change(PgAttribute.foregroundNew(0xcccc, 0x0000, 0x0000));
+				label.setAttributes(attrs);
+
+				break;
+			case State.Disabled:
+				button.setSensitive(false);
+				Label label = cast(Label) builder.getObject(id ~ "Label");
+				label.setLabel("Disabled");
+				label.setAttributes(new PgAttributeList());
+
+				break;
+			default:
+				break;
+		}
+	}
+
+	void communicationButtonCallback(Button b) {
+		if(b == wifiButton) {
+			wifiState = wifiState is State.Online ? State.Offline : State.Online;
+			setConnectionButtonState(wifiButton, wifiState);
+		} else if (b == bluetoothButton) {
+			bluetoothState = bluetoothState is State.Online ? State.Offline : State.Online;
+			setConnectionButtonState(bluetoothButton, bluetoothState);
+		} else if (b == usbButton) {
+			usbState = usbState is State.Online ? State.Offline : State.Online;
+			setConnectionButtonState(usbButton, usbState);
+		}
+	}
+
+	void sidebarOnChange(ListBoxRow lbr, ListBox lb) {
+		trace("sidebarOnChange()");
+		backButton.setVisible(true);
+		if(lb == generalBar) {
+			if(lbr == gConfigOption) {
+					sidebar.setVisibleChild(configBar);
+					mainInterface.setVisibleChild(configInterface);
+					configInterface.setVisibleChild(cGeneralInterface);
+			} else if(lbr == gPluginsOption) {
+					sidebar.setVisibleChild(pluginsBar);
+					mainInterface.setVisibleChild(pluginsInterface);
+			}
+		} else if(lb == pluginsBar) {
+
+		} else if(lb == configBar) {
+			if(lbr == cGeneralOption) {
+				configInterface.setVisibleChild(cGeneralInterface);
+			} else if(lbr == cConnectionOption) {
+				configInterface.setVisibleChild(cConnectionInterface);
+			} else if(lbr == cPluginsOption) {
+				configInterface.setVisibleChild(cPluginsInterface);
+			} else if(lbr == cDevicesOption) {
+				configInterface.setVisibleChild(cDevicesInterface);
+			}
+		}
+	}
+
+	void backButtonCallback(Button b) {
+		trace("backButtonCallback()");
+		sidebar.setVisibleChild(generalBar);
+		mainInterface.setVisibleChild(generalInterface);
+		backButton.setVisible(false);
+	}
+
+	void ccEnableBoxes(ToggleButton tb) {
+		State state = tb.getActive() ? State.Offline : State.Disabled;
+		if(tb == ccwEnableCheck) {
+			wifiState = state;
+			setConnectionButtonState(wifiButton, wifiState);
+		} else if(tb == ccbEnableCheck) {
+			bluetoothState = state;
+			setConnectionButtonState(bluetoothButton, bluetoothState);
+		} else if(tb == ccuEnableCheck) {
+			usbState = state;
+			setConnectionButtonState(usbButton, usbState);
+		}
 	}
 }
