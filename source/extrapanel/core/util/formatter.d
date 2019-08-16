6b6a023e785e32b533ee3f385494149eda63b208
Ev1lbl0w <ricasubtil@gmail.com>
2019-08-16 16:13:31 +0100

    formatter: added helper methods to show colored output
    Signed-off-by: Ev1lbl0w <ricasubtil@gmail.com>

--- source/extrapanel/core/util/formatter.d
+++ source/extrapanel/core/util/formatter.d
@@ -11,3 +11,15 @@ public static string italic(string text) {
 public static string monospaced(string text) {
 	return "<tt>" ~ text ~ "</tt>";
 }
+
+public static string consoleGreen(string text) {
+	return "\033[0;32m" ~ text ~ "\033[1;37m";
+}
+
+public static string consoleYellow(string text) {
+	return "\033[1;33m" ~ text ~ "\033[1;37m";
+}
+
+public static string consoleRed(string text) {
+	return "\033[0;31m" ~ text ~ "\033[1;37m";
+}
