d2e4bf8b1c8543cc6874c7a032fcb6a1ccce633d
Ev1lbl0w <ricasubtil@gmail.com>
2019-09-28 14:30:19 +0100

    codecov: fixed code coverage not reporting the results

--- tools/dub.sh
+++ tools/dub.sh
@@ -2,14 +2,40 @@
 
 set -e
 
-PACKAGES="
-core
-ui
-manager
-tray
-daemon
+PACKAGES="$(find source/extrapanel -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)"
+
+coverage_tmp_folder=".dub-coverage"
+
+if [[ "$*" == *"--coverage"* ]]
+then
+	mkdir -p $coverage_tmp_folder
+	DUB_COVERAGE="1"
+fi
+
+exit_trap() {
+    if [[ $DUB_COVERAGE == "1" ]]
+	then
+		USELESS_DUB_COVERAGE_OUTPUT="
+-home-
+-root-
+-tmp-
+..-
 "
+		for useless in $USELESS_DUB_COVERAGE_OUTPUT; do
+			rm -rf -- "$useless"*.lst
+		done
+
+		mv -f "$coverage_tmp_folder"/* .
+		rm -rf "$coverage_tmp_folder"
+	fi
+}
+
+trap exit_trap EXIT
 
 for subprojects in $PACKAGES; do
-	dub $@ :$subprojects
+	dub "$@" ":$subprojects"
+	if [[ "$DUB_COVERAGE" == "1" ]]
+	then
+		find . -name "source-extrapanel-$subprojects-*.lst" -print0 | xargs -0r mv -t "$coverage_tmp_folder"
+	fi
 done
